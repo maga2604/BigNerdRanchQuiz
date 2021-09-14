9bf23f3a160883a40698ae97b0a6e18bbcf19f00
Maga <maga.rajic2604@gmail.com>
2021-09-14 08:59:34 -0700

    First commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Maga Rajic on 14.09.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+    
+    }
+    @IBAction func showAnswer(_ sender: UIButton) {
+    
+    }
+    
+    
+}
+
